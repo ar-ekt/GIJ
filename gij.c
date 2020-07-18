34c63e254022eaec0c4a82475c54424c391aece4
ar-ekt <57006850+ar-ekt@users.noreply.github.com>
2020-07-18 06:10:35 +0430

    add comment - fix some bugs

--- gij.c
+++ gij.c
@@ -5,9 +5,24 @@
 
 #define MAX_ARRAY_SIZE 300
 
+/**
+ * a temp array for saving situation of files
+**/
+
 int temp_Situation[MAX_ARRAY_SIZE];
+
+/**
+ * number of commits!!!!!!!!
+**/
+
 int number_Of_Commit;
+
+/**
+ * length of status dictionary
+**/
+
 int status_Len;
+
 struct dict{
 	int last_Ver;
 	int last_Situation;
@@ -16,6 +31,14 @@ struct dict{
 	char name[MAX_ARRAY_SIZE];
 } status[MAX_ARRAY_SIZE];
 
+/** 
+ * This function get name of a file and return format of it in a string
+ * 
+ * @param file is name of the given file
+ * @param format format of the given file
+ *
+ */
+ 
 void get_Format(char file[], char format[]){
 	int len = strlen(file);
 	int index = len;
@@ -25,12 +48,32 @@ void get_Format(char file[], char format[]){
 		format[index] = file[index+len-lenF];
 }
 
+/** 
+ * This function make one copy of the given file in wanted address
+ * 
+ * @param origin address of original file
+ * @param fileO name of original file
+ * @param destination the address to make a copy of file
+ * @param fileD name of copy file
+ *
+ */
+
 void make_Copy(char origin[], char fileO[], char destination[], char fileD[]){
 	char command[MAX_ARRAY_SIZE];
-	sprintf(command, "copy %s\\%s %s\\%s > E#R#R#O#R.txt", origin, fileO, destination, fileD);
+	sprintf(command, "copy %s\\%s %s\\%s", origin, fileO, destination, fileD);
+	puts(command);
 	system(command);
-	remove("E#R#R#O#R.txt");
+//	remove("E#R#R#O#R.txt");
 }
+
+/** 
+ * This function find the index of given file in dictionry
+ * 
+ * @param file name of the file given for search
+ *
+ * @return index of file in dictionary if file is exsit otherwise return -1
+ *
+ */
 
 int dict_Index(char file[]){
 	int index;
@@ -40,9 +83,24 @@ int dict_Index(char file[]){
 	return -1;
 }
 
+/** 
+ * This function get a string and clean it by memset function
+ * 
+ * @param string ...
+ *
+ */
+
 void clean(char string[]){
 	memset(string, '\0', sizeof(string));
 }
+
+/** 
+ * This function get a address and replace all backslash(\) with hash(#) 
+ * 
+ * @param name1 given address
+ * @param name2 converted address
+ *
+ */
 
 void lash_2_Hash(char name1[], char name2[]){
 	int len = strlen(name1);
@@ -55,6 +113,14 @@ void lash_2_Hash(char name1[], char name2[]){
 	name2[index] = '\0';
 }
 
+/** 
+ * This function get a address and replace all hash(#) with backslash(\)
+ * 
+ * @param name1 given address
+ * @param name2 converted address
+ *
+ */
+
 void hash_2_Lash(char name1[], char name2[]){
 	int len = strlen(name1);
 	int index;
@@ -65,6 +131,13 @@ void hash_2_Lash(char name1[], char name2[]){
 			name2[index] = name1[index];
 	name2[index] = '\0';
 }
+
+/** 
+ * This function update status-dictionry with ststus-file
+ * 
+ * @param path address of main folder
+ *
+ */
 
 void status_Get(char path[]){
 	int index, indexS;
@@ -88,6 +161,14 @@ void status_Get(char path[]){
 	}
 	fclose(vF);
 }
+
+/** 
+ * This function update ststus-file with status-dictionry
+ * 
+ * @param path address of main folder
+ * @param minus number of deleted files after reset
+ *
+ */
 
 void status_Update(char path[], int minus){
 	int index, indexS;
@@ -115,6 +196,13 @@ void status_Update(char path[], int minus){
 	
 	fclose(vF);
 }
+
+/** 
+ * This function update situation of files after every commit
+ * 
+ * @param path address of main folder
+ *
+ */
 
 void situation_Update(char path[]){
 	char name_Lashed[MAX_ARRAY_SIZE];
@@ -145,6 +233,14 @@ void situation_Update(char path[]){
 	}
 }
 
+/** 
+ * This function reset commits to given Id
+ * 
+ * @param path address of main folder
+ * @param commitId ...
+ *
+ */
+
 void reset(char path[], int commitId){
 	int index, indexV, ban=0;
 	char path_Des[MAX_ARRAY_SIZE];
@@ -154,6 +250,7 @@ void reset(char path[], int commitId){
 	char name_Ori[MAX_ARRAY_SIZE];
 	char formatV[MAX_ARRAY_SIZE];
 	char command[MAX_ARRAY_SIZE];
+	
 	status_Get(path);
 	
 	// delete descriptions
@@ -182,6 +279,7 @@ void reset(char path[], int commitId){
 				sprintf(path_Des, "%s\\vc_data\\files\\%s\\%d.txt", path, status[index].name, indexV);
 				remove(path_Des);
 			}
+			
 			sprintf(path_Des, "%s\\vc_data\\files\\%s\\%d%s", path, status[index].name, indexV-1, formatV);
 			remove(path_Des);
 			
@@ -195,10 +293,9 @@ void reset(char path[], int commitId){
 					remove(path_Ori);
 			}
 			
-			sprintf(path_Ori, "%s\\vc_data\\files\\%s, path", status[index].name);
+			sprintf(path_Ori, "%s\\vc_data\\files\\%s", path, status[index].name);
 			sprintf(name_Ori, "%d%s", status[index].all_Ver[commitId-1], formatV);
 			make_Copy(path_Ori, name_Ori, path, name_Lashed);
-			
 		}
 		else if(status[index].all_Situation[commitId-1] == 3){
 			
@@ -227,8 +324,18 @@ void reset(char path[], int commitId){
 	status_Update(path, ban);
 }
 
+/** 
+ * This function commit selected files and updates their diffs
+ * 
+ * @param path address of main folder
+ * @param selected array of files for commit
+ * @param numberOS number of selected files
+ *
+ * in this function OS means "of selected file\s"
+ *
+ */
+
 void commit(char path[], char selected[][MAX_ARRAY_SIZE], int numberOS){
-	puts(path);
 	int indexOS;
 	int verOS;
 	int indInVer;
@@ -256,7 +363,6 @@ void commit(char path[], char selected[][MAX_ARRAY_SIZE], int numberOS){
 		if(indInVer == -1){
 			verOS = 1;
 			
-			puts(pathOS);
 			mkdir(pathOS);
 			
 			sprintf(buffer, "%d%s", verOS, formatOS);
@@ -288,7 +394,7 @@ void commit(char path[], char selected[][MAX_ARRAY_SIZE], int numberOS){
 			}
 			
 			status[indInVer].last_Ver += 1;
-			temp_Situation[status_Len] = 1;
+			temp_Situation[indInVer] = 1;
 		}
 	}
 	
@@ -296,6 +402,13 @@ void commit(char path[], char selected[][MAX_ARRAY_SIZE], int numberOS){
 	number_Of_Commit++;
 	status_Update(path, 0);
 }
+
+/** 
+ * This function show allthing about commits
+ * 
+ * @param path address of main folder
+ *
+ */
 
 void log_(char path[]){
 	char pathD[MAX_ARRAY_SIZE];
@@ -307,9 +420,9 @@ void log_(char path[]){
 	
 	system("cls");
 	
+	printf("\nLogs\n\n");
 	if(number_Of_Commit == 0)
 		printf("nothing to show\n");
-	
 	for(index=0; index<number_Of_Commit; index++){
 		printf("commit-Id: %d\n", index+1);
 		sprintf(pathD, "%s\\vc_data\\descriptions\\%d.txt", path, index+1);
@@ -320,33 +433,61 @@ void log_(char path[]){
 	}
 }
 
-void status_(char path[], char selected[][MAX_ARRAY_SIZE], int selected_Len){
+/** 
+ * This function show allthing about files
+ * 
+ * @param path address of main folder
+ *
+ */
+
+void status_(char path[]){
 	char name_Lashed[MAX_ARRAY_SIZE];
 	char conv_Situation[][MAX_ARRAY_SIZE] = {"deleted", "unchanged", "changed", "recently"};
 	int index;
 	
 	status_Get(path);
 	
-	printf("\n\nStatus of files\n\n");
+	system("cls");
+	
+	printf("\nStatus of files\n\n");
 	if(status_Len == 0)
 		printf("nothing to show\n");
 	for(index=0; index<status_Len; index++)
 		if(-1 <= status[index].last_Situation && status[index].last_Situation <= 2){
 			hash_2_Lash(status[index].name, name_Lashed);
-			printf("%s: \t %s\n", name_Lashed, conv_Situation[status[index].last_Situation+1]);
+			printf("%s \n    %s\n\n", name_Lashed, conv_Situation[status[index].last_Situation+1]);
 		}
 }
+
+/** 
+ * This function show selected files
+ * 
+ * @param selected array of files
+ * @param selected_Len number of selected files
+ *
+ */
 
 void show_Selected(char selected[][MAX_ARRAY_SIZE], int selected_Len){
 	int index;
 	system("cls");
 	
-	printf("Selected files\n\n");
+	printf("Selected files\n\n\n");
 	if(selected_Len == 0)
 		printf("no file selected\n");
 	for(index=0; index<selected_Len; index++)
-		puts(selected[index]);
+		printf("%s\n\n", selected[index]);
 }
+
+/** 
+ * This function check a file is in selected files or not if yes remove it
+ * 
+ * @param file the name of given file to check
+ * @param selected array of files
+ * @param selected_Len number of selected files
+ *
+ * @return 1 if file exsit in selected files otherwise 0
+ *
+ */
 
 int try_Unselect(char file[], char selected[][MAX_ARRAY_SIZE], int selected_Len){
 	int index, shift;
@@ -360,12 +501,34 @@ int try_Unselect(char file[], char selected[][MAX_ARRAY_SIZE], int selected_Len)
 	return 0;
 }
 
+/** 
+ * This function check the given file is directory or not
+ * 
+ * @param pre path of the 
+ * @param file name of file
+ *
+ * @return 1 if file is directory otherwise 0
+ *
+ */
+
 int is_Dir(char pre[], char file[]){
 	if(chdir(file))
 		return 0;
 	chdir(pre);
 	return 1;
 }
+
+/** 
+ * This function search all of main directory and ite sub-directories to find files and select them
+ * 
+ * @param path address of main folder
+ * @param cur_Dir path of where is being searched
+ * @param selected array of files
+ * @param selected_Len number of selected files
+ *
+ * @return updated number of selected files
+ *
+ */
 
 int select_All(char path[], char cur_Dir[], char selected[][MAX_ARRAY_SIZE], int selected_Len){
 	char file_Name[MAX_ARRAY_SIZE];
@@ -395,6 +558,16 @@ int select_All(char path[], char cur_Dir[], char selected[][MAX_ARRAY_SIZE], int
 	return selected_Len;
 }
 
+/** 
+ * This function check the given file exist or not
+ * 
+ * @param path address of main folder
+ * @param file name of file will be checked
+ *
+ * @return 1 if file exist otherwise 0
+ *
+ */
+
 int check_File(char path[], char file[]){
 	char pathF[MAX_ARRAY_SIZE];
 	sprintf(pathF, "%s\\%s", path, file);
@@ -407,6 +580,15 @@ int check_File(char path[], char file[]){
 	return 0;
 }
 
+/** 
+ * This function gets a string and checks if it's all numerical
+ * 
+ * @param number is the string needed to be checked
+ *
+ * @return integer 0 if "number" string is numerical otherwise 1
+ *
+ */
+
 int isNumerical(char number[]){
 	int i=0;
 	
@@ -416,6 +598,11 @@ int isNumerical(char number[]){
 	return 0; // 1 for ok
 }
 
+/** 
+ * This function show where user is
+ *
+ */
+
 void showPath(){
 	char cwd[MAX_ARRAY_SIZE];
 	if(getcwd(cwd, sizeof(cwd)))
@@ -424,7 +611,12 @@ void showPath(){
 	    printf("Unable to show path ...");
 }
 
-void show_Screen(char path[]){
+/** 
+ * This function update main screen
+ *
+ */
+
+void show_Screen(){
 	system("cls");
 	showPath();
 	char gij[]= "\n\n\n\n\n"
@@ -437,15 +629,7 @@ void show_Screen(char path[]){
 				"\t\t\t\t\t\\__________||_________|\\________/ \n"
 				"\n\n";
 	puts(gij);
-	printf("\t\t\t\t\t");
-}
-
-void make_Path(char path_list[][MAX_ARRAY_SIZE], int pl, char path[]){
-	int index;
-	for(index=0; index<pl; index++){
-		strcat(path, "\\");
-		strcat(path, path_list[index]);
-	}
+	printf("\t\t\t\t    >>> ");
 }
 
 int main(){
@@ -456,13 +640,15 @@ int main(){
 	char selected[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE];
 	char description[MAX_ARRAY_SIZE];
 	char buffer[MAX_ARRAY_SIZE];
+	
 	int selected_Len = 0;
 	int input_Len;
 	int index = 0;
 	int init = 0;
+	
 	while(1){
 		
-		show_Screen(path);
+		show_Screen();
 		gets(line);
 		char *split = strtok(line, " ");
 		input_Len = 0;
@@ -473,13 +659,15 @@ int main(){
 		
 		if(input_Len == 0)
 			continue;
+		
+		getcwd(path, sizeof(path));
 			
 		if(!chdir("vc_data")){
 			chdir("..");
+			if(!init)
+				status_Get(path);
 			init = 1;
 		}
-		
-		getcwd(path, sizeof(path));
 		
 		printf("\n\n");
 		
@@ -518,7 +706,7 @@ int main(){
 					"select -all\t\t\t select all files in current directory and sub-directories\n\n"
 					"unselect <file1> <file2> ...\t unselect given files\n\n"
 					"unselect -all\t\t\t unselect all selected files\n\n"
-					"selected\t show selected files\n\n"
+					"selected\t\t\t show selected files\n\n"
 					"commit <description>\t\t commit selected files\n\n"
 					"log\t\t\t\t show allthings about commits\n\n"
 					"reset <commit-Id>\t\t bla bla bla\n\n"
@@ -549,7 +737,7 @@ int main(){
 			else if(!init)
 				printf("no permission!\n");
 			else
-				status_(path, selected, selected_Len);
+				status_(path);
 		}
 		
 		else if(!strcasecmp(input[0], "select")){
@@ -573,7 +761,7 @@ int main(){
 				for(index=1; index<input_Len; index++){
 					if(check_File(path, input[index])){
 						strcpy(selected[selected_Len++], input[index]);
-						printf("%s file selected succesfully\n\n", selected[selected_Len-1]);
+						printf("%s selected succesfully\n\n", selected[selected_Len-1]);
 					}
 					else
 						printf("cannot find %s!\n\n", input[index]);
@@ -590,8 +778,8 @@ int main(){
 				if(input_Len != 2)
 					printf("unknown command!\n");
 				else{
-					printf("all selected files unselected succesfully\n");
 					selected_Len = 0;
+					printf("all selected files unselected succesfully\n");
 				}
 			}
 			else{
@@ -641,8 +829,7 @@ int main(){
 				for(index=0; index<selected_Len; index++){
 					printf("%s commited succesfully\n", selected[index]);
 					version_Of_S = dict_Index(selected[index]);
-					printf("last version: %d\n", status[version_Of_S].last_Ver);
-					printf("last situation: %d\n\n", conv_Situation[status[version_Of_S].last_Situation+1]);
+					printf("last version: %d\n\n", status[version_Of_S].last_Ver);
 				}
 				selected_Len = 0;
 			}
@@ -662,11 +849,11 @@ int main(){
 				printf("unknown command!\n");
 			else if(!init)
 				printf("no permission!\n");
-			if(isNumerical(input[1])){
+			if(!isNumerical(input[1])){
 				int commitId = atoi(input[1]);
 				if(1 <= commitId && commitId < number_Of_Commit){
 					reset(path, commitId);
-					printf("reser done succesfully\n");
+					printf("reset done succesfully\n");
 				}
 				else if(commitId == 0){
 					int n = 1;
